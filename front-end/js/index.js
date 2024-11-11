2dd20c72ef9fdd1b17c0f6ff39e7be2e1f62ed9b
gherkins05 <153504167+gherkins05@users.noreply.github.com>
2024-11-11 13:15:05 +0000

    Added async version of promise method

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -27,3 +27,40 @@ function loginAttempt() {
         }
     });
 }
+
+// Recreated loginAttempt using async and await
+
+/*
+
+async function loginAttempt() {
+    const username = document.getElementById('username-input').value;
+    const password = document.getElementById('password-input').value;
+
+    try {
+        const response = await fetch('http://localhost:3000/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        if (!response.ok) { throw new Error(`HTTP error! status: ${response.status}`); }
+
+        const data = await response.json();
+
+        if (data.token) {
+            localStorage.setItem('token', data.token);
+            window.location.href = '/home';
+        } else {
+            const error = document.getElementById('login-error');
+            error.innerHTML = data.message;
+            console.log("Error logging in: ", data.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+*/
